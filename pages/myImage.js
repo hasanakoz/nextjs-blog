3b96c46044e7301e23d840bcd845def5d11c2d3d
hasan <hasanakoz1986@gmail.com>
2023-07-27 23:51:08 +0300

    static file added

---
+++ pages/myImage.js
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export const MyImage = () => (
+  <Image src="/images/profile.jpg" height={144} width={130} alt="hasan" />
+);
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
+import { MyImage } from "../myImage";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>first-post</h1>
+      <MyImage />
+      <Head>
+        <h1>First Post</h1>
+      </Head>
       <h2>
         <Link href="/">Back to Home</Link>
       </h2>
